217658b247f0464951efb22eea4ded47c492e2c8
willt <91817538+swillswindle@users.noreply.github.com>
2021-12-18 21:40:19 -0800

    losing when throwing rock no longer increments the win counter

--- script.js
+++ script.js
@@ -7,71 +7,67 @@ function rps(max) {
 let again = true;
 
 let stats = {
-wins: 0,
-losses: 0,
-draws: 0,
-}
+  wins: 0,
+  losses: 0,
+  draws: 0,
+};
 
 while (again) {
-let userChoice = prompt("Enter R, P, or S.");
+  let userChoice = prompt("Enter R, P, or S.");
 
-let cpuChoice = rps(3);
+  let cpuChoice = rps(3);
 
-let result = "";
+  let result = "";
 
-if (cpuChoice === 0) {
-  result = "R";
-}
+  if (cpuChoice === 0) {
+    result = "R";
+  } else if (cpuChoice === 1) {
+    result = "P";
+  } else if (cpuChoice === 2) {
+    result = "S";
+  }
 
-else if (cpuChoice === 1) {
-  result = "P";
-}
+  console.log(cpuChoice);
+  console.log(userChoice);
 
-else if (cpuChoice === 2) {
-  result = "S";
-}
-
-console.log(cpuChoice);
-console.log(userChoice);
-
-if (userChoice.toUpperCase() === result) {
-  alert("It's a draw!");
-  stats.draws++
-}
-
-else if (userChoice.toUpperCase() === "R") {
+  if (userChoice.toUpperCase() === result) {
+    alert("It's a draw!");
+    stats.draws++;
+  } else if (userChoice.toUpperCase() === "R") {
     if (result === "P") {
-        alert("You Lose!")
-        stats.losses++
+      alert("You Lose!");
+      stats.losses++;
+    } else if (result === "S") {
+      alert("You Win!");
+      stats.wins++;
     }
-    else if (result === "S")
-        alert("You Win!")
-        stats.wins++
-}
-else if (userChoice.toUpperCase() === "P") {
+  } else if (userChoice.toUpperCase() === "P") {
     if (result === "S") {
-        alert("You Lose!")
-        stats.losses++
+      alert("You Lose!");
+      stats.losses++;
+    } else if (result === "R") {
+      alert("You Win!");
+      stats.wins++;
     }
-    else if (result === "R") {
-        alert("You Win!")
-        stats.wins++
-    }
-}
-else if (userChoice.toUpperCase() === "S") {
+  } else if (userChoice.toUpperCase() === "S") {
     if (result === "R") {
-        alert("You Lose!")
-        stats.losses++
+      alert("You Lose!");
+      stats.losses++;
+    } else if (result === "P") {
+      alert("You Win!");
+      stats.wins++;
     }
-    else if (result === "P") {
-        alert("You Win!")
-        stats.wins++
-    }
+  }
+  alert(
+    "Wins: " +
+      stats.wins +
+      " Losses: " +
+      stats.losses +
+      " Draws: " +
+      stats.draws
+  );
 
+  again = confirm("Would you like to play again?");
 }
-alert("Wins: " + stats.wins + " Losses: " + stats.losses + " Draws: "+ stats.draws);
 
-again = confirm("Would you like to play again?");
-}
-
-alert("Thank you for playing!")
+alert("Thank you for playing!");
